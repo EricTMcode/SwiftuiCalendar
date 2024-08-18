d3edd5d3b9cb916dc3aa95dac1a8083e99d959e9
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2024-08-18 06:48:16 +0200

    Initial Commit

---
+++ SwiftuiCalendar/SwiftuiCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiCalendarApp.swift
+//  SwiftuiCalendar
+//
+//  Created by Eric on 18/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
